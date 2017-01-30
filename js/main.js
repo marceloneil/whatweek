70f00ae00a059b1c518603a4b8026a69319224fa
Marcel O'Neil <marcel@marceloneil.com>
2017-01-30 09:55:10 -0500

    Fix month change

--- js/main.js
+++ js/main.js
@@ -1,7 +1,31 @@
 var curr = new Date();
+var leapyear = 0;
+if (curr.getFullYear() % 4 === 0) {
+  leapyear = 1;
+}
+var months = [31, 28 + leapyear, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
 var wednesday = curr.getDate() - curr.getDay() + 3;
-var firstday = new Date(curr.setDate(wednesday - 1)).toISOString();
-var lastday = new Date(curr.setDate(wednesday)).toISOString();
+var tuesday = wednesday - 1;
+var wednesdaymonth = curr.getMonth();
+var tuesdaymonth = wednesdaymonth;
+if (wednesday > months[curr.getMonth()]) {
+  wednesday -= months[curr.getMonth()];
+  wednesdaymonth += 1;
+  tuesdaymonth += 1;
+  if (wednesday == 1) {
+    tuesday = months[curr.getMonth()];
+    tuesdaymonth -= 1;
+  }
+}
+var tuesdayDate = new Date(curr);
+tuesdayDate.setDate(tuesday);
+tuesdayDate.setMonth(tuesdaymonth);
+var wednesdayDate = new Date(curr);
+wednesdayDate.setDate(wednesday);
+wednesdayDate.setMonth(wednesdaymonth);
+var firstday = tuesdayDate.toISOString();
+var lastday = wednesdayDate.toISOString();
 
 var api = 'AIzaSyArpdb_OtqLF7KE6IUTscWNDXR4iJoN4mw';
 var id = 'utschools.ca_9o570sdn27bd1f9e2ojhai6p2g@group.calendar.google.com';
@@ -13,10 +37,10 @@ calendar.onreadystatechange = function() {
     if (calendar.readyState == 4 && calendar.status == 200) {
         var items = JSON.parse(calendar.response).items;
         for (var i = 0; i < items.length; i++) {
-            if (items[i].summary == 'B Day') {
+            if (items[i].summary.includes('B Day')) {
                 week = 'B';
                 break;
-            } else if (items[i].summary == 'A Day') {
+            } else if (items[i].summary.includes('A Day')) {
                 week = 'A';
                 break;
             }
